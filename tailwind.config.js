0c9da9321ac901b9f4dc9057ea8899423c1c54b8
unknown <guptalakshya8383@gmail.com>
2025-02-03 15:44:18 +0530

    login page css updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Make sure to include all the directories where Tailwind will be used
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
